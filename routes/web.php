668301e4d5310ced0e619807fcd029d6ef838a87
Sovia Lajuba <sovialajuba0108@gmail.com>
2024-05-18 04:10:49 -0700

    update route

--- routes/web.php
+++ routes/web.php
@@ -2,17 +2,18 @@
 
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
+// Route untuk halaman login menggunakan LoginController dengan method index
+Route::get('/login', [App\Http\Controllers\Auth\LoginController::class, 'index'])->name('login');
+Route::post('/logout', [App\Http\Controllers\Auth\LoginController::class, 'logout'])->name('logout');
 
+// Route untuk root URL yang mengembalikan view 'welcome'
 Route::get('/', function () {
     return view('welcome');
 });
+
+Auth::routes();
+//
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+
