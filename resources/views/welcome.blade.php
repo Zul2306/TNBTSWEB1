65bb94ea415922f10e6a9dffcad8b49ad365547d
sovia0108 <sovialajuba2003@gmail.com>
2024-05-28 03:32:17 +0700

    Update tampilan login & LandingPage

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -22,7 +22,7 @@
                 transition: background-color 0.3s ease;
             }
             .navbar.scrolled {
-                background-color: black !important;
+                background-color: rgba(44, 44, 44, 0.726) !important;
             }
             .navbar-transparent {
                 background-color: transparent !important;
@@ -30,7 +30,7 @@
         </style>
         <nav class="navbar navbar-expand-lg navbar-light fixed-top navbar-transparent" id="mainNav">
             <div class="container px-4 px-lg-5">
-                <a class="navbar-brand" href="#page-top">Start Bootstrap</a>
+                <a class="navbar-brand" href="#">TNBTS</a>
                 <button class="navbar-toggler navbar-toggler-right" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
                     Menu
                     <i class="fas fa-bars"></i>
@@ -52,39 +52,24 @@
                 height: 100vh;
             }
         </style>
+         <style>.center-container { display: flex; justify-content: center; align-items: center; height: auto; } .center-container img { max-width: 50%; height: auto; }</style>
         <header class="masthead">
             <div class="container px-4 px-lg-5 d-flex h-100 align-items-center justify-content-center">
                 <div class="d-flex justify-content-center">
                     <div class="text-center">
-                        <h1 class="mx-auto my-0 text-uppercase">ANJAY SLEBEW</h1>
-                        <h2 class="text-white-50 mx-auto mt-2 mb-5">A free, responsive, one page Bootstrap theme created by Start Bootstrap.</h2>
-                        <a class="btn btn-primary" href="{{ route('login') }}">Get Started</a>
+                        <div class=" center-container "><img class="img-fluid" src="{{asset('img/logo.png')}}" alt="..." /></div>
+                        <h2 class="text-white-50 mx-auto mt-2 mb-5">Laporkan Bencana yang terjadi di sekitar anda dengan tombol dibawah ini </h2>
+                        <a class="btn btn-primary" href="{{ route('login') }}">BUAT LAPORAN</a>
                     </div>
                 </div>
             </div>
         </header>
-        <!-- About-->
-        <section class="about-section text-center" id="about">
-            <div class="container px-4 px-lg-5">
-                <div class="row gx-4 gx-lg-5 justify-content-center">
-                    <div class="col-lg-8">
-                        <h2 class="text-white mb-4">Built with Bootstrap 5</h2>
-                        <p class="text-white-50">
-                            Grayscale is a free Bootstrap theme created by Start Bootstrap. It can be yours right now, simply download the template on
-                            <a href="https://startbootstrap.com/theme/grayscale/">the preview page.</a>
-                            The theme is open source, and you can use it for any purpose, personal or commercial.
-                        </p>
-                    </div>
-                </div>
-                <img class="img-fluid" src="{{ asset('img/ipad.png') }}" alt="..." />
-            </div>
-        </section>
         <!-- Projects-->
         <section class="projects-section bg-light" id="projects">
             <div class="container px-4 px-lg-5">
                 <!-- Featured Project Row-->
                 <div class="row gx-0 mb-4 mb-lg-5 align-items-center">
-                    <div class="col-xl-8 col-lg-7"><img class="img-fluid mb-3 mb-lg-0" src="{{ asset('img/bg-masthead.jpg') }}" alt="..." /></div>
+                    <div class="col-xl-8 col-lg-7"><img class="img-fluid mb-3 mb-lg-0" src="{{ asset('img/bromo.jpeg') }}" alt="..." /></div>
                     <div class="col-xl-4 col-lg-5">
                         <div class="featured-text text-center text-lg-left">
                             <h4>Shoreline</h4>
