9f61a3ba1d037db9dff7ef4e6875d205f886e02f
Zul2306 <148838510+Zul2306@users.noreply.github.com>
2024-05-28 20:19:46 +0700

    update 3

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -19,9 +19,9 @@
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <link rel="apple-touch-icon" sizes="76x76" href="../assets/img/apple-icon.png">
-    <link rel="icon" type="image/png" href="{{asset('../assets/img/favicon.png')}}">
+    <link rel="icon" type="image/png" href="{{asset('img/logo.png')}}">
     <title>
-        TNBTSWEB1
+        BROTERU
     </title>
     <!--     Fonts and icons     -->
     <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700" rel="stylesheet" />
@@ -41,8 +41,8 @@
         <div class="sidenav-header">
             <i class="fas fa-times p-3 cursor-pointer text-secondary opacity-5 position-absolute end-0 top-0 d-none d-xl-none" aria-hidden="true" id="iconSidenav"></i>
             <a class="navbar-brand m-0" href=" https://demos.creative-tim.com/argon-dashboard/pages/dashboard.html " target="_blank">
-                <img src="../assets/img/logo-ct-dark.png" class="navbar-brand-img h-100" alt="main_logo">
-                <span class="ms-1 font-weight-bold">Argon Dashboard 2</span>
+                <img src="{{asset('img/logo.png')}}" class="navbar-brand-img h-100" alt="main_logo">
+                <span class="ms-1 font-weight-bold">SiporBoteru</span>
             </a>
         </div>
         <hr class="horizontal dark mt-0">
