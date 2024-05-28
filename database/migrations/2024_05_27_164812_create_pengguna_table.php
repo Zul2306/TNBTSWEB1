2ba7a2af5edce3ceffb46578ff376906911d78be
Zul2306 <148838510+Zul2306@users.noreply.github.com>
2024-05-28 20:05:27 +0700

    push 4

---
+++ database/migrations/2024_05_27_164812_create_pengguna_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('pengguna', function (Blueprint $table) {
+            $table->id();
+            $table->string('nama_pengguna');
+            $table->string('surel');
+            $table->string('kata_sandi');
+            $table->enum('peran', ['pengguna', 'admin']);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('pengguna');
+    }
+};
